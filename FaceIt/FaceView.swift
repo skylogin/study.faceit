bf39b62144e48586cad83b51f09cdd47fb37f020
Chanho Jung <skylogin@gmail.com>
2018-09-26 23:26:02 +0900

    first commit

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Awesome S on 26/09/2018.
+//  Copyright © 2018 Awesome S. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        
+        
+    }
+
+}
